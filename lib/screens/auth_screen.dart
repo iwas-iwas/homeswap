91542e8ab5568b6d2e226ed190ace50b5c5a8309
Iwas <iiwas@uni-potsdam.de>
2020-12-21 23:28:52 +0100

    improve auth error handling

--- lib/screens/auth_screen.dart
+++ lib/screens/auth_screen.dart
@@ -1,17 +1,18 @@
-import 'package:conspacesapp/screens/Welcome/welcome_screen.dart';
+//import 'package:conspacesapp/screens/Welcome/welcome_screen.dart';
 import 'package:conspacesapp/widgets/auth/auth_form_anon.dart';
-import 'package:conspacesapp/widgets/database_service.dart';
-import 'package:firebase_core/firebase_core.dart';
+// import 'package:conspacesapp/widgets/database_service.dart';
+// import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import '../widgets/auth/auth_form.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'dart:io';
-import 'package:firebase_storage/firebase_storage.dart';
+// import 'dart:io';
+// import 'package:firebase_storage/firebase_storage.dart';
 
-import 'Welcome/components/body.dart';
+// import 'Welcome/components/body.dart';
+import 'package:purchases_flutter/purchases_flutter.dart';
 
 class AuthScreen extends StatefulWidget {
   bool isAnon;
@@ -29,24 +30,6 @@ class _AuthScreenState extends State<AuthScreen> {
   Future sendPasswordResetEmail(String email) async {
     return _auth.sendPasswordResetEmail(email: email);
   }
-
-  // Future deleteUser(String email, String password) async {
-  //   try {
-  //     final user = FirebaseAuth.instance.currentUser;
-  //     AuthCredential credentials =
-  //         EmailAuthProvider.credential(email: email, password: password);
-  //     print(user);
-  //     UserCredential result =
-  //         await user.reauthenticateWithCredential(credentials);
-  //     await DatabaseService(user: user)
-  //         .deleteuser(); // called from database class
-  //     await result.user.delete();
-  //     return true;
-  //   } catch (e) {
-  //     print(e.toString());
-  //     return null;
-  //   }
-  // }
 
   // Create Anonymous User
   Future signInAnonymously() {
@@ -90,6 +73,8 @@ class _AuthScreenState extends State<AuthScreen> {
         'token': token,
         'image_url': null,
       });
+
+      await Purchases.identify(currentUser.uid);
     } on PlatformException catch (err) {
       var message = 'An error occurred, pelase check your credentials!';
 
@@ -134,6 +119,8 @@ class _AuthScreenState extends State<AuthScreen> {
           email: email,
           password: password,
         );
+
+        await Purchases.identify(authResult.user.uid);
       } else {
         authResult = await _auth.createUserWithEmailAndPassword(
           email: email,
@@ -163,6 +150,8 @@ class _AuthScreenState extends State<AuthScreen> {
           'token': token,
           'image_url': null,
         });
+
+        await Purchases.identify(authResult.user.uid);
       }
     } on PlatformException catch (err) {
       var message = 'You have entered an invalid email or password.';
@@ -172,27 +161,77 @@ class _AuthScreenState extends State<AuthScreen> {
 
       if (err.message != null) {
         message = err.message;
+        print("platformexception: " + message);
       }
 
       Scaffold.of(ctx).showSnackBar(
         SnackBar(
           content: Text(message),
-          backgroundColor: Theme.of(ctx).errorColor,
-        ),
-      );
-      setState(() {
-        _isLoading = false;
-      });
-    } catch (err) {
-      Scaffold.of(ctx).showSnackBar(
-        SnackBar(
-          content: Text('You have entered an invalid email or password.'),
           backgroundColor: Color(0xFF4845c7),
         ),
       );
       setState(() {
         _isLoading = false;
       });
+      // } on FirebaseAuthException catch (e) {
+      //   // weak password wird nur getrhowd bei <6 letters, was sowieso im backend abgedeckt ist.
+      //   // if (e.code == 'weak-password') {
+      //   //   print('The password provided is too weak bro.');
+      //   //   Scaffold.of(ctx).showSnackBar(
+      //   //     SnackBar(
+      //   //       content: Text('Your password is too weak.'),
+      //   //       backgroundColor: Color(0xFF4845c7),
+      //   //     ),
+      //   //   );
+      //   // }
+      //   if (e.code == 'email-already-in-use') {
+      //     Scaffold.of(ctx).showSnackBar(
+      //       SnackBar(
+      //         content:
+      //             Text("The email address is already in use by another account."),
+      //         backgroundColor: Color(0xFF4845c7),
+      //       ),
+      //     );
+      //     print('The account already exists for that email bro.');
+      //     setState(() {
+      //       _isLoading = false;
+      //     });
+      //   }
+    } catch (err) {
+      if (err.code == 'email-already-in-use') {
+        Scaffold.of(ctx).showSnackBar(
+          SnackBar(
+            content:
+                Text("The email address is already in use by another account."),
+            backgroundColor: Color(0xFF4845c7),
+          ),
+        );
+        setState(() {
+          _isLoading = false;
+        });
+      } else if ((err.code == 'too-many-requests')) {
+        Scaffold.of(ctx).showSnackBar(
+          SnackBar(
+            duration: const Duration(seconds: 5),
+            content: Text(
+                'Access to this account has been temporarily disabled due to many failed login attempts. You can immediately restore it by resetting your password or you can try again later.'),
+            backgroundColor: Color(0xFF4845c7),
+          ),
+        );
+        setState(() {
+          _isLoading = false;
+        });
+      } else {
+        Scaffold.of(ctx).showSnackBar(
+          SnackBar(
+            content: Text('You have entered an invalid email or password.'),
+            backgroundColor: Color(0xFF4845c7),
+          ),
+        );
+        setState(() {
+          _isLoading = false;
+        });
+      }
     }
   }
 
