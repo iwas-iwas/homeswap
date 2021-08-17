d8034f6eb5bd5211fcfc783c814c9019856fbf5d
Iwas <iiwas@uni-potsdam.de>
2021-08-17 21:15:00 +0200

    remove unused code

--- lib/main.dart
+++ lib/main.dart
@@ -7,10 +7,8 @@ import './screens/tabs_screen.dart';
 import './screens/auth_screen.dart';
 import './screens/slpash_screen.dart';
 import 'package:firebase_core/firebase_core.dart';
-//import 'package:device_preview/device_preview.dart';
 import 'package:purchases_flutter/purchases_flutter.dart';
 import 'package:flutter/services.dart';
-import './credentials.dart';
 import 'package:firebase_analytics/firebase_analytics.dart';
 import 'package:firebase_analytics/observer.dart';
 
@@ -22,13 +20,6 @@ void main() async {
   runApp(MyApp());
 }
 
-// void main() {
-//   runApp(DevicePreview(
-//     builder: (context) => MyApp(),
-//     enabled: !kReleaseMode, // remove devicepreview if app is in release mode
-//   ));
-// }
-
 class MyApp extends StatefulWidget {
   @override
   _MyAppState createState() => _MyAppState();
@@ -38,35 +29,7 @@ class _MyAppState extends State<MyApp> {
   @override
   void initState() {
     super.initState();
-    //initPlatformState();
   }
-
-  // Future<void> initPlatformState() async {
-  //   // DocumentSnapshot query =
-  //   //     await FirebaseFirestore.instance.collection(QUERY).doc(QUERY3).get();
-
-  //   //print(query.data()['back']);
-
-  //   appData.isPro = false;
-
-  //   await Purchases.setDebugLogsEnabled(true);
-  //   await Purchases.setup(RABBIT_API_KEY);
-  //   //await Purchases.setup(query.data()[QUERY2]);
-
-  //   //print(query.data()['back']);
-
-  //   PurchaserInfo purchaserInfo;
-  //   try {
-  //     purchaserInfo = await Purchases.getPurchaserInfo();
-  //     if (purchaserInfo.entitlements.all['all_features'] != null) {
-  //       appData.isPro = purchaserInfo.entitlements.all['all_features'].isActive;
-  //     } else {
-  //       appData.isPro = false;
-  //     }
-  //   } on PlatformException catch (e) {
-  //     print(e);
-  //   }
-  // }
 
   @override
   Widget build(BuildContext context) {
